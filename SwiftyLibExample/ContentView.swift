dbf9bd28c7284f6fa7c5c233493513245f1af131
Sajan Shrestha <sajanshrestharm11@gmail.com>
2021-01-19 13:31:11 -0500

    first

---
+++ SwiftyLibExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftyLibExample
+//
+//  Created by Sajan Shrestha on 1/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftyLibExample/SwiftyLibExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLibExampleApp.swift
+//  SwiftyLibExample
+//
+//  Created by Sajan Shrestha on 1/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLibExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
